5f77eded274da306ad5404ae24c69a22a859c24d
widha893 <widha.rizqika.prasetya@mail.ugm.ac.id>
2025-02-18 19:15:48 +0700

    add roll and yaw data

--- hitl/hitl/data_plotter.py
+++ hitl/hitl/data_plotter.py
@@ -3,22 +3,22 @@ import matplotlib.pyplot as plt
 
 # Load data
 file_path = 'pitch step.csv'  # Ganti dengan nama file CSV yang sesuai
-df = pd.read_csv('pitch step.csv')
+df = pd.read_csv('Yaw step.csv')
 
 # Plot data
 plt.figure(figsize=(10, 5))
-plt.plot(df["Time"], df["Pitch (Sim)"], label="Sudut Pitch (Derajat)", color="orange")  # Mengubah warna menjadi hijau
-plt.plot(df["Time"], df["Pitch (Real)"], label="Respon Pitch (Derajat)", color="green")  # Mengubah warna menjadi biru
+plt.plot(df["Time"], df["Yaw (sim)"], label="Respon Sudut Yaw (Derajat)", color="orange")  # Mengubah warna menjadi hijau
+# plt.plot(df["Time"], df["Yaw (real)"], label="Respon pada Lingkungan Nyata (Derajat)", color="green")  # Mengubah warna menjadi biru
 
 # Tambahkan batas toleransi atas dan bawah
-plt.axhline(y=1.5, color="red", linestyle="--", label="Batas Toleransi Atas")
-plt.axhline(y=-1.5, color="red", linestyle="--", label="Batas Toleransi Bawah")
+plt.axhline(y=2.5, color="red", linestyle="--", label="Batas Toleransi Atas")
+plt.axhline(y=-2.5, color="red", linestyle="--", label="Batas Toleransi Bawah")
 plt.axhline(y=0.0, color="black", linestyle="--", label="Setpoint")
 
 # Label dan judul
 plt.xlabel("Waktu (Detik)")
-plt.ylabel("Sudut Pitch (Derajat)")
-plt.title("Rspon Sudut Pitch pada Pengujian di Lingkungan Nyata")
+plt.ylabel("Sudut Yaw (Derajat)")
+plt.title("Respon Sudut Yaw pada Lingkungan Simulasi")
 plt.legend()
 plt.grid()
 
